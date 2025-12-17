92ad9e2d9681acfa51dbe59ba3e05cbfc264f0cd
Joker8-h <arlisalfonsopertuzvillareal@gamil.com>
2025-12-17 10:36:00 -0500

    cors arreglado

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const documentacionRoutes = require('./ROUTES/DocumentacionRoutes');
 //uso de cors
 app.use(cors(
     {
-        origin: 'http://localhost:5173',
+        origin: '*',
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         allowedHeaders: ['Content-Type', 'Authorization']
     }
