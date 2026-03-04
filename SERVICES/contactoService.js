24d79a07c443f2ee4ddd8b28f82295f6e280ad49
Joker8-h <arlisalfonsopertuzvillareal@gamil.com>
2026-03-04 09:39:06 -0500

    soporte

--- SERVICES/contactoService.js
+++ SERVICES/contactoService.js
@@ -1,18 +1,26 @@
 const nodemailer = require('nodemailer');
+
 const transporter = nodemailer.createTransport({
-    
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
 const enviarCorreoContacto = async ({ nombre, correo, tipo, mensaje }) => {
+
+  await transporter.verify(); // para ver si conecta
+
   const mailOptions = {
     from: `"Formulario Web" <${process.env.EMAIL_USER}>`,
     to: process.env.EMAIL_USER,
-    subject: `Nuevo mensaje de contacto - ${tipo}`,
+    subject: `Nuevo mensaje - ${tipo}`,
     html: `
       <h2>Nuevo mensaje desde la web</h2>
       <p><strong>Nombre:</strong> ${nombre}</p>
